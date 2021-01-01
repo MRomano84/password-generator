1fac04a867b7e22404346a4b97a97ebe725ff9b8
Matthew Romano <mathiasrok@gmail.com>
2021-01-01 15:23:57 -0500

    trying a different way

--- script.js
+++ script.js
@@ -1,57 +1,12 @@
-function password() {
-    let upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let lowerChar = "abcdefghijklmnopqrstuvwxyz";
-    let numChar = "1234567890";
-    let symChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-    let allChar = upperChar + lowerChar + numChar + symChar;
+//DOM elements
+let lowerBox = document.getElementById("lowerCase");
+let upperBox = document.getElementById("upperCase");
+let symBox = document.getElementById("symbols");
+let numbBox = document.getElementById("numbers");
+let chosenLength = document.getElementById("slider");
 
-    let lowerBox = document.getElementById("lowerCase");
-    let upperBox = document.getElementById("upperCase");
-    let symBox = document.getElementById("symbols");
-    let numbBox = document.getElementById("numbers");
-    let chosenLength = document.getElementById("slider");
-    let newPassword = document.getElementById("generate");
-
-
-
-    console.log(password(chosenLength.value, allChar));
-    display.value = password(chosenLength.value, allChar)
-
-
-    document.getElementById("generate").addEventListener('click', function() {
-        if (lowerBox.checked) {
-            lowerChar += newPassword
-        } else {
-            lowerChar += nothing
-        }
-
-        if (upperBox.checked) {
-            upperChar += newPassword
-        } else {
-            upperChar += nothing
-        }
-        if (numbBox.checked) {
-            numChar += newPassword
-        } else {
-            numChar += nothing
-        }
-        if (symBox.checked) {
-            symChar += newPassword
-        } else {
-            symChar += nothing
-        }
-    });
-
-
-
-
-    function password(l, characters) {
-        let pword = "";
-
-        for (var i = 0; i < l; i++) {
-            pword = pword + characters.charAt(Math.floor(Math.random() * characters.length));
-        }
-
-        return pword;
-    }
-};
+//Characters for password
+let upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerChar = "abcdefghijklmnopqrstuvwxyz";
+let numChar = "1234567890";
+let symChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
