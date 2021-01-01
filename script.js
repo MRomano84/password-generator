37e8c9aacf7642371aa2e8be8a203a92c822f9f5
Matthew Romano <mathiasrok@gmail.com>
2021-01-01 15:35:22 -0500

    functions to get characters

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //DOM elements
 let lowerBox = document.getElementById("lowerCase");
 let upperBox = document.getElementById("upperCase");
-let symBox = document.getElementById("symbols");
-let numbBox = document.getElementById("numbers");
+let symBox = document.getElementById("symbol");
+let numbBox = document.getElementById("number");
 let chosenLength = document.getElementById("slider");
 
 //Characters for password
@@ -10,3 +10,21 @@ let upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let lowerChar = "abcdefghijklmnopqrstuvwxyz";
 let numChar = "1234567890";
 let symChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+//Functions to get characters
+function getLowerCaseChar() {
+    return lowLetters[Math.floor(Math.random() * lowLetters.length)];
+}
+
+function getUpperCaseChar() {
+    return uppLetters[Math.floor(Math.random() * uppLetters.length)];
+}
+
+function getSymbol() {
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function getNumber() {
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
