b69796588ab868f2e3c255dd7f888d87d1d5cabc
Matthew Romano <mathiasrok@gmail.com>
2021-01-01 16:04:24 -0500

    added functions

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //DOM elements
-let lowerBox = document.getElementById("lowerCase");
-let upperBox = document.getElementById("upperCase");
-let symBox = document.getElementById("symbol");
-let numbBox = document.getElementById("number");
+let lowerBox = document.getElementById("lowerCase").checked;
+let upperBox = document.getElementById("upperCase").checked;
+let symBox = document.getElementById("symbol").checked;
+let numbBox = document.getElementById("number").checked;
 let chosenLength = document.getElementById("slider");
+let yourPassword = document.getElementById("yourPassword")
 
 //Characters for password
 let upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -28,3 +29,28 @@ function getNumber() {
     return numbers[Math.floor(Math.random() * numbers.length)];
 }
 
+
+
+function generatePassword() {
+    let len = chosenLength.value;
+    let password = "";
+
+    for(let i = 0; i < len; i++) {
+        let p = generateP();
+        password += p;
+    }
+    
+    yourPassword.innerText = password;
+}
+
+
+
+function generateP() {
+    let xp = [];
+    lowerBox.checked && xp.push(getLowerCaseChar());
+    upperBox.checked && xp.push(getUpperCaseChar());
+    symBox.checked && xp.push(getSymbol());
+    numbBox.checked && xp.push(getNumber());
+
+    return xp[Math.floor(Math.random() * xp.length)];
+}
