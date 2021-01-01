59ff45f2aac9bc3cef2d536ea707289b4381706b
Matthew Romano <mathiasrok@gmail.com>
2021-01-01 18:00:18 -0500

    fixed id's and added functions

--- script.js
+++ script.js
@@ -1,56 +1,63 @@
 //DOM elements
-let lowerBox = document.getElementById("lowerCase").checked;
-let upperBox = document.getElementById("upperCase").checked;
-let symBox = document.getElementById("symbol").checked;
-let numbBox = document.getElementById("number").checked;
-let chosenLength = document.getElementById("slider");
-let yourPassword = document.getElementById("yourPassword")
+const yourPassword = document.getElementById("display");
+const chosenLength = document.getElementById("slider");
+const upperBox = document.getElementById("upperCase");
+const lowerBox = document.getElementById("lowerCase");
+const numbBox = document.getElementById("number");
+const symBox = document.getElementById("symbol");
+const genButton = document.getElementById("generate");
 
-//Characters for password
-let upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let lowerChar = "abcdefghijklmnopqrstuvwxyz";
-let numChar = "1234567890";
-let symChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//Characters to make password with
+const upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerChar = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+=[]";
 
 //Functions to get characters
-function getLowerCaseChar() {
-    return lowLetters[Math.floor(Math.random() * lowLetters.length)];
+function getLowercase() {
+  return lowerChar[Math.floor(Math.random() * lowerChar.length)];
 }
 
-function getUpperCaseChar() {
-    return uppLetters[Math.floor(Math.random() * uppLetters.length)];
-}
-
-function getSymbol() {
-    return symbols[Math.floor(Math.random() * symbols.length)];
+function getUppercase() {
+  return upperChar[Math.floor(Math.random() * upperChar.length)];
 }
 
 function getNumber() {
-    return numbers[Math.floor(Math.random() * numbers.length)];
+  return numbers[Math.floor(Math.random() * numbers.length)];
 }
 
+function getSymbol() {
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
 
-
+//Function to generate Password
 function generatePassword() {
-    let len = chosenLength.value;
+    const len = chosenLength.value;
     let password = "";
 
-    for(let i = 0; i < len; i++) {
-        let p = generateP();
-        password += p;
+    for (let i = 0; i < len; i++) {
+        const x = generateX();
+        password += x;
     }
-    
-    yourPassword.innerText = password;
+
+    display.innerText = password;
 }
 
-
-
-function generateP() {
-    let xp = [];
-    lowerBox.checked && xp.push(getLowerCaseChar());
-    upperBox.checked && xp.push(getUpperCaseChar());
-    symBox.checked && xp.push(getSymbol());
+//Adding checked checkbox characters to array of potential password characters
+function generateX() {
+    const xp = [];
+    upperBox.checked && xp.push(getUppercase());
+    lowerBox.checked && xp.push(getLowercase());
     numbBox.checked && xp.push(getNumber());
+    symBox.checked && xp.push(getSymbol());
 
-    return xp[Math.floor(Math.random() * xp.length)];
+    if (xp.length == 0) {
+        return alert("Please Select at Least 1 Character Type.");
+    }
+
+  return xp[Math.floor(Math.random() * xp.length)];
+
 }
+
+//Executing the function to get password
+genButton.addEventListener("click", generatePassword);
